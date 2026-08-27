0952d90bcb337fa3cf1374a1e67bf7d6a19d3e47
Nils <nils.gellert@outlook.de>
2026-08-27 14:49:41 +0200

    fix1

--- src/Platformer.java
+++ src/Platformer.java
@@ -30,7 +30,7 @@ public class Platformer extends JFrame {
 		JFileChooser fc = new JFileChooser();
 		fc.setCurrentDirectory(new File("./"));
 		fc.setDialogTitle("Select input image");
-		FileFilter filter = new FileNameExtensionFilter("Level image (.bmp)", "bmp", "png");
+		FileFilter filter = new FileNameExtensionFilter("Level image (.bmp)", "bmp");
 		fc.setFileFilter(filter);
 		int result = fc.showOpenDialog(this);
 		File selectedFile = new File("");
