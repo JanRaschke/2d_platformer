bfd0fe1d29e85190c04e2bd6dd5cdc1bc7f281c2
Nils <nils.gellert@outlook.de>
2026-08-27 11:50:45 +0200

    step1_final_v2

--- src/Platformer.java
+++ src/Platformer.java
@@ -1,5 +1,7 @@
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
@@ -15,15 +17,41 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 public class Platformer extends JFrame {
 	@Serial
 	private static final long serialVersionUID = 5736902251450559962L;
+	private Level level;
+	private int cameraX = 0;
 
 	BufferedImage levelImg;
+
 	public Platformer() {
-		//exit program when window is closed
-		this.addWindowListener(new WindowAdapter(){
-			public void windowClosing(WindowEvent e){
-					System.exit(0);
+		// exit program when window is closed
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
 			}
 		});
+
+		this.level = new Level();
+		this.addKeyListener(new KeyAdapter() {
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (level == null || level.getRenderedLevel() == null) {
+					return;
+				}
+				if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+					cameraX = Math.max(0, cameraX - 20);
+					repaint();
+				} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+					int maxScroll = level.getRenderedLevel().getWidth() - 1000;
+					cameraX = Math.min(maxScroll, cameraX + 20);
+					repaint();
+				}
+
+			}
+
+		});
+
+		this.setTitle("Platformer");
 
 		JFileChooser fc = new JFileChooser();
 		fc.setCurrentDirectory(new File("./"));
@@ -44,7 +72,7 @@ public class Platformer extends JFrame {
 		try {
 			levelImg = ImageIO.read(selectedFile);
 
-			this.setBounds(0, 0, levelImg.getWidth()+16, levelImg.getHeight()+39);
+			this.setBounds(0, 0, 1000 + 16, 350 + 39);
 			this.setVisible(true);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -54,7 +82,12 @@ public class Platformer extends JFrame {
 
 	@Override
 	public void paint(Graphics g) {
-		Graphics2D g2d = (Graphics2D)g;
-		g2d.drawImage(levelImg, 8, 31, this);
+		Graphics2D g2d = (Graphics2D) g;
+		if (level != null && level.getRenderedLevel() != null) {
+			BufferedImage visibleLevel = level.getRenderedLevel().getSubimage(cameraX, 0, 1000, 350);
+			g2d.drawImage(visibleLevel, 8, 31, this);
+
+		}
+
 	}
 }
