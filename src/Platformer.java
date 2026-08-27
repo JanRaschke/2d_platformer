62eb77be1d030110dc7745aaf9075428fccc972e
JanRaschke <janphilipraschke@gmail.com>
2026-08-27 10:28:34 +0200

    Init task 2 solutions.

--- src/Platformer.java
+++ src/Platformer.java
@@ -1,43 +1,60 @@
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.geom.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
-import java.io.Serial;
-import javax.swing.JFrame;
-import javax.imageio.ImageIO;
 import java.io.File;
-import java.io.IOException;
+import java.io.Serial;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Platformer extends JFrame {
 	@Serial
 	private static final long serialVersionUID = 5736902251450559962L;
-	private BufferedImage image;
 
+	BufferedImage levelImg;
 	public Platformer() {
+		//exit program when window is closed
+		this.addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e){
+					System.exit(0);
+			}
+		});
+
+		JFileChooser fc = new JFileChooser();
+		fc.setCurrentDirectory(new File("./"));
+		fc.setDialogTitle("Select input image");
+		FileFilter filter = new FileNameExtensionFilter("Level image (.bmp)", "bmp");
+		fc.setFileFilter(filter);
+		int result = fc.showOpenDialog(this);
+		File selectedFile = new File("");
+
+		if (result == JFileChooser.APPROVE_OPTION) {
+			selectedFile = fc.getSelectedFile();
+			System.out.println("Selected file: " + selectedFile.getAbsolutePath());
+		} else {
+			dispose();
+			System.exit(0);
+		}
+
 		try {
-			image = ImageIO.read(new File("testImg.bmp"));
-		} catch (IOException e) {
+			levelImg = ImageIO.read(selectedFile);
+
+			this.setBounds(0, 0, levelImg.getWidth()+16, levelImg.getHeight()+39);
+			this.setVisible(true);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setTitle("HelloWorld");
-		this.setBounds(0, 0, 320, 240);
-		this.setVisible(true);
 	}
 
 	@Override
 	public void paint(Graphics g) {
-		Graphics2D g2d = (Graphics2D) g; // cast to Graphics2D
-		g2d.drawImage(image, 0, 0, this);
-		/*
-		 * Line2D.Double line = new Line2D.Double(20.0, 50.0, 50.0, 200.0);
-		 * g2d.draw(line);
-		 * Rectangle2D.Double rect = new Rectangle2D.Double(100.0, 50.0, 60.0, 80.0);
-		 * g2d.draw(rect); // also try g2d.fill(rect);
-		 * Ellipse2D.Double circle = new Ellipse2D.Double(200.0, 100.0, 80.0, 80.0);
-		 * g2d.draw(circle); // also try g2d.fill(circle);
-		 */
+		Graphics2D g2d = (Graphics2D)g;
+		g2d.drawImage(levelImg, 8, 31, this);
 	}
-
 }
